dee8559ae5b6333c29188e1aa34d3108f4c5773b
RenanZenaro <renanzenaro@hotmail.com>
2024-11-05 20:43:07 -0300

    05/11

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
+  const mapRef = useRef(null)
+
+  useEffect(() => {
+    (
+      async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync()
+        if (status !== 'granted') {
+          setErrorMsg("Permissão para acessar a localização foi negada")
+          return
+        }
+
+        const subscription = await Location.watchPositionAsync(
+          {
+            accuracy: Location.Accuracy.High,
+            timeInterval: 1000,
+            distanceInterval: 1
+          },
+          (newLocation) => {
+            const { latitude, longitude } = newLocation.coords
+            setLocation({ latitude, longitude })
+
+            if (mapRef.current) {
+              mapRef.current.animateToRegion({
+                latitude,
+                longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+              })
+            }
+          }
+        )
+        return () => subscription.remove()
+      }
+    )()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={{
+          latitude: location ? location.latitude : -23.5505,
+          longitude: location ? location.longitude : -46.6333,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showUserLocation={true}
+      >
+        {location && (
+          <Marker coordinate={{ latitude: location.latitude, longitude: location.longitude }} />
+        )}
+
+      </MapView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+})
+
+App()
